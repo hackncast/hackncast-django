976c6b18d760a6c5c42b2c729c43db5bc32bc460
Magnun Leno <magnun.leno@gmail.com>
2016-08-21 00:39:06 -0300

    Adds redirects for login, logout and signup

--- hnc/urls.py
+++ hnc/urls.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import include, url
+from django.views.generic import RedirectView
+from django.core.urlresolvers import reverse_lazy
 
 urlpatterns = [
     url(r'^', include('apps.core.urls')),
+    url(r'^login/$',
+        RedirectView.as_view(url=reverse_lazy('account_login'))),
+    url(r'^logout/$',
+        RedirectView.as_view(url=reverse_lazy('account_logout'))),
+    url(r'^signup/$',
+        RedirectView.as_view(url=reverse_lazy('account_signup'))),
     url(r'^user/', include('apps.user.urls')),
 ]

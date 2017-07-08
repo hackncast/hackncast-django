e3d3af96458028b63a0e6377d8497d190f77e410
Magnun Leno <magnun.leno@gmail.com>
2017-07-08 00:42:26 -0300

    Removing old vendor files

--- frontend/src/js/user/base.js
+++
@@ -1,5 +0,0 @@
-//=include src/js/base.js
-//=include vendor/semantic-ui/components/visibility.js
-//=include vendor/semantic-ui/components/dropdown.js
-//=include vendor/semantic-ui/components/sidebar.js
-//=include vendor/semantic-ui/components/checkbox.js
